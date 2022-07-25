1ee63ab2b2e9c4be3bdeb8c8e7aaa7c476357079
Anhfpt2404 <109655246+Anhfpt2404@users.noreply.github.com>
2022-07-25 12:54:29 +0700

    Add files via upload

---
+++ assignment 2 cach 2.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a =");
+	scanf("%d",&a);
+	
+	int b;
+	printf("Nhap b =");
+	scanf("%d",&b);
+	
+	int c;
+	printf("Nhap c =");
+	scanf("%d",&c);	
+	
+	if (a>b>c){
+		//yes
+		printf("Max: %d",a);
+	}else{
+		//no
+		if(b>c){
+			//yes
+			printf("Max: %d",b);
+		}else{
+			//no
+			printf("Max: %d",c);
+			
+		}
+	}
+	
+}
